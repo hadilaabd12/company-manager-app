6f8351f109e5474e0af6030f28578ab8d1b725bd
Hadil Abdelaoui <hadil.abdelaoui@issatso.u-sousse.tn>
2022-11-17 11:42:30 +0100

    Starting creation of models
    Created employee, team and work arrangement models.

--- app/cmanager/models.py
+++ app/cmanager/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Employee model with name, id, team, hourly rate
+class Employee(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    hourlyRate = models.FloatField()
+    workArrangement = models.ForeignKey(WorkArrangement, on_delete=models.CASCADE)
+    def salary(self):
+        return workArrangement * hourlyRate
+
+class Team(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+
+class WorkArrangement(models.Model):
+    type = ( 
+        'Fulltime',
+        'Part-time, 25%',
+        'Part-time, 50%',
+        'Part-time, 75%'
+    )
