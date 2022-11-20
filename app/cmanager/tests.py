29eda2e8adf46931a8508a4c5c8f3d08a4d5cd90
Hadil Abdelaoui <hadil.abdelaoui@issatso.u-sousse.tn>
2022-11-20 20:44:00 +0100

    Adding additional tests

--- app/cmanager/tests.py
+++ app/cmanager/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import WorkArrangement, Employee, Team, TeamLeader, TeamEmployee
+from .models import Person, WorkArrangement, Employee, Team
 from django.utils import timezone
 
 
@@ -7,49 +7,63 @@ from django.utils import timezone
 class PersonTest(TestCase):
 
     def create_wa(self, name="Person 1"):
-        return Employee.objects.create(type=type, created_at=timezone.now())
+        return Employee.objects.create(name=name, created_at=timezone.now())
 
     def test_wa_creation(self):
-        w = self.create_wa()
-        self.assertTrue(isinstance(w, Employee))
+        w = self.create_wa(name="Person 1")
+        self.assertTrue(isinstance(w, Person))
         self.assertEqual(w.__unicode__(), w.name)
 
 
 class WarrangementTest(TestCase):
 
-    def create_wa(self, type='40'):
+    def create_wa(self, type=40):
         return WorkArrangement.objects.create(
             type=type,
             created_at=timezone.now()
         )
 
     def test_wa_creation(self):
-        w = self.create_wa()
+        wa = 40
+        w = self.create_wa(wa)
         self.assertTrue(isinstance(w, WorkArrangement))
         self.assertEqual(w.__unicode__(), w.type)
 
 
 class EmployeeTest(TestCase):
 
-    def create_employee(self, name="Employee 1", hourly_rate=10):
-        return Employee.objects.create(
-            name=name,
-            hourly_rate=hourly_rate,
-            created_at=timezone.now()
-        )
+    workArrangement=WorkArrangement(type=40)
+    WorkArrangement.save(workArrangement)
+    def create_employee(
+        self,
+        name="Employee 1",
+        hourly_rate=10,
+        workArrangement=workArrangement, 
+        created_at=timezone.now()):
+            return Employee.objects.create(
+                name=name,
+                hourly_rate=hourly_rate,
+                workArrangement=workArrangement,
+                created_at=timezone.now()
+            )
 
     def test_employee_creation(self):
-        w = self.create_employee()
+        
+        w = self.create_employee(
+            name="Employee 1",
+            hourly_rate=10,
+            workArrangement=WorkArrangement(type=40)
+        )
         self.assertTrue(isinstance(w, Employee))
         self.assertEqual(w.__unicode__(), w.name)
 
 
 class TeamTest(TestCase):
 
-    def create_team(self, type='E'):
+    def create_team(self, type='Employee'):
         return Team.objects.create(type=type, created_at=timezone.now())
 
     def test_team_creation(self):
-        w = self.create_team()
+        w = self.create_team(type='Employee')
         self.assertTrue(isinstance(w, Team))
-        self.assertEqual(w.__unicode__(), w.title)
+        self.assertEqual(w.__unicode__(), w.type)
--- app/cmanager/urls.py
+++ app/cmanager/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import PersonViewSet, EmployeeViewSet, WarrangementViewSet, TeamViewSet
 
 urlpatterns = [
     path('person', PersonViewSet.as_view()),
